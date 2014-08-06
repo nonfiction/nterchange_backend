0b7c741865e410ec9e381702c60894a8f6ad4b4b
Andy VanEe <1andyvanee@gmail.com>
2014-08-06 12:05:25 -0600

    Conditional object constructor for old php

--- lib/n_object.php
+++ lib/n_object.php
@@ -11,28 +11,37 @@
  * send a note to license@php.net so we can mail you a copy immediately.
  *
  * @category   	Object
- * @author     	Tim Glen <tim@nonfiction.ca>
- * @copyright  	2005-2007 nonfiction studios inc.
+ * @author     	Tim Glen <tim@nonfiction.ca>, Andy VanEe <andy@nonfiction.ca>
+ * @copyright  	2005-2014 nonfiction studios inc.
  * @license    	http://www.php.net/license/3_01.txt  PHP License 3.01
  * @version    	SVN: $Id$
  * @link       	http://www.nterchange.com/
  * @since      	File available since Release 3.0
  */
-class Object {
-	function Object() {
-		$args = func_get_args();
-		// register_shutdown_function(array(&$this, '__destruct'));
-		call_user_func_array(array(&$this, '__construct'), $args);
-	}
+if (substr(phpversion(),0,1) == 4) {
+	class Object {
+		function Object() {
+			$args = func_get_args();
+			// register_shutdown_function(array(&$this, '__destruct'));
+			call_user_func_array(array(&$this, '__construct'), $args);
+		}
 
-	function __construct() {
-	}
+		function __construct() {
+		}
 
-	function __destruct() {
-	}
+		function __destruct() {
+		}
 
-	function toString() {
-		return get_class($this);
+		function toString() {
+			return get_class($this);
+		}
+	}
+} else {
+	class Object {
+		function __construct(){}
+
+		function toString() {
+			return get_class($this);
+		}
 	}
 }
-?>
